cc393757f3c8fbb278006d5fc8760116960877aa
Natalya Manzha <natalia@manzha.ru>
2024-06-07 20:01:42 +0400

    TECH-9-navigation-part1

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -2,20 +2,43 @@ package ru.practicum.android.diploma.root.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.practicum.android.diploma.BuildConfig
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Пример использования access token для HeadHunter API
-        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isVisible = when (destination.id) {
+                R.id.searchFragment,
+                R.id.favoritesFragment,
+                R.id.teamFragment -> true
+
+                else -> false
+            }
+
+            binding.bottomNavigationView.isVisible = isVisible
+            binding.line.isVisible = isVisible
+        }
     }
+}
 
-    private fun networkRequestExample(accessToken: String) {
+/*
+* Пример использования access token для HeadHunter API
+* networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+* private fun networkRequestExample(accessToken: String) {
         // ...
     }
-
-}
+* */
