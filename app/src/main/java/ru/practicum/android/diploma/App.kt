5d3811b27822166eb9fad45dd5b7230674b76c5a
RodionKirillov <rodion.44454@gmail.com>
2024-06-10 11:17:45 +0300

    DETAIL-43-Add-Room-database

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import ru.practicum.android.diploma.di.favoritesModule
 import ru.practicum.android.diploma.di.networkClientModule
 import ru.practicum.android.diploma.search.di.searchRepositoryModule
 
@@ -15,7 +16,8 @@ class App : Application() {
             androidContext(this@App)
             modules(
                 networkClientModule,
-                searchRepositoryModule
+                searchRepositoryModule,
+                favoritesModule
             )
         }
     }
