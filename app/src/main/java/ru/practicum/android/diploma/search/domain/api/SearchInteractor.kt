25af716770d668ac4ccb7ebb407f7151513514c4
Natalya Manzha <natalia@manzha.ru>
2024-06-11 19:55:30 +0400

    SEARCH-58 corrections

--- app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchInteractor.kt
@@ -1,9 +1,5 @@
 package ru.practicum.android.diploma.search.domain.api
 
-
-import ru.practicum.android.diploma.search.domain.models.Errors
-import ru.practicum.android.diploma.search.domain.models.SearchResult
-import ru.practicum.android.diploma.search.domain.models.Vacancies
 import ru.practicum.android.diploma.search.domain.models.SearchResult
 import ru.practicum.android.diploma.search.domain.models.VacanciesSearchRequest
 
