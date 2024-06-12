42afe39861e6c869f4608e5acae7277b4db0b860
SAndreyV1683 <ya.sboevandrey@yandex.ru>
2024-06-12 17:39:31 +0300

    DIPLOMA-83 detekt errors corrected

--- app/src/main/java/ru/practicum/android/diploma/vacancy/ui/models/VacancyDetailsUIState.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/ui/models/VacancyDetailsUIState.kt
@@ -4,11 +4,12 @@ import ru.practicum.android.diploma.search.domain.models.Errors
 import ru.practicum.android.diploma.vacancy.domain.models.VacancyDetails
 
 sealed class VacancyDetailsUIState {
-    data object Loading: VacancyDetailsUIState()
+    data object Loading : VacancyDetailsUIState()
     data class Content(
         val details: VacancyDetails
-    ): VacancyDetailsUIState()
+    ) : VacancyDetailsUIState()
+
     data class Error(
         val errors: Errors?
-    ): VacancyDetailsUIState()
+    ) : VacancyDetailsUIState()
 }
