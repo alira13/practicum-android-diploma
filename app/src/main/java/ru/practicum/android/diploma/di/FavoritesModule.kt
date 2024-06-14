f675c2ac940cdae616d516e9c51819ed1ed13dce
Alira13 <bezglasnaya@gmail.com>
2024-06-14 15:56:25 +0300

    Add connection viewModel-interactor

--- app/src/main/java/ru/practicum/android/diploma/di/FavoritesModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/FavoritesModule.kt
@@ -3,8 +3,11 @@ package ru.practicum.android.diploma.di
 import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.favorites.data.converters.VacancyConverterDB
 import ru.practicum.android.diploma.favorites.data.db.AppDatabase
 import ru.practicum.android.diploma.favorites.data.db.impl.VacancyRepositoryImpl
+import ru.practicum.android.diploma.favorites.domain.api.VacancyRepository
+import ru.practicum.android.diploma.favorites.domain.api.VacansyInteractor
 import ru.practicum.android.diploma.favorites.domain.impl.VacancyInteractorImpl
 
 val favoritesModule = module {
@@ -14,12 +17,13 @@ val favoritesModule = module {
             .build()
     }
 
-    single {
-        VacancyRepositoryImpl(
-            get(),
-            get()
-        )
+    single<VacancyRepository> {
+        VacancyRepositoryImpl(get(), get())
     }
 
-    single { VacancyInteractorImpl(get()) }
+    single<VacansyInteractor> {
+        VacancyInteractorImpl(get())
+    }
+
+    single { VacancyConverterDB() }
 }
--- app/src/main/java/ru/practicum/android/diploma/favorites/presentation/FavoritesUiState.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/presentation/FavoritesUiState.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.favorites.presentation
 import ru.practicum.android.diploma.search.domain.models.VacancyPreview
 
 sealed interface FavoritesUiState {
-    data object Default : FavoritesUiState
     data object Empty : FavoritesUiState
     data object Failure : FavoritesUiState
     data class Content(val list: List<VacancyPreview>) : FavoritesUiState
