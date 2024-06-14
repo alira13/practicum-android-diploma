70a38b08ae605c2adef4fa3e45f9ccd399582fce
Ksenia Bezglasnaia <bezglasnaya@gmail.com>
2024-06-14 11:20:15 +0300

    Update SearchFragment.kt
    Delete unused toast

--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
 import android.view.inputmethod.InputMethodManager
