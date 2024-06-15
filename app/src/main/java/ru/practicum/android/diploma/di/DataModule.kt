e853f38e3c1d254845e044cbe87d64449165a1e4
RodionKirillov <rodion.44454@gmail.com>
2024-06-15 15:35:24 +0300

    BUG-95 Di fix

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,17 +1,48 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.favorites.data.converters.VacancyConverterDB
+import ru.practicum.android.diploma.favorites.data.db.AppDatabase
+import ru.practicum.android.diploma.search.data.VacancyConverter
 import ru.practicum.android.diploma.search.data.api.HHApiService
 import ru.practicum.android.diploma.search.data.api.NetworkClient
 import ru.practicum.android.diploma.search.data.network.RetrofitNetworkClient
+import ru.practicum.android.diploma.share.data.ExternalNavigator
+import ru.practicum.android.diploma.vacancy.data.DetailsConverter
 
 const val BASE_URL = "https://api.hh.ru/"
 
-val networkClientModule = module {
+val dataModule = module{
+
+    factory { DetailsConverter() }
+
+    single { VacancyConverterDB() }
+
+    factory { VacancyConverter(androidContext()) }
+
+    single {
+        ExternalNavigator(
+            context = get()
+        )
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(
+            apiService = get()
+        )
+    }
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 
     single<HHApiService> {
         val client = OkHttpClient.Builder().addInterceptor(
@@ -27,9 +58,5 @@ val networkClientModule = module {
             .build()
 
         retrofit.create(HHApiService::class.java)
-    }
-
-    single<NetworkClient> {
-        RetrofitNetworkClient(get())
     }
 }
