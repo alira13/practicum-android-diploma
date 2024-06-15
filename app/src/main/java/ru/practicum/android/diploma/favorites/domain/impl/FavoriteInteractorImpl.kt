7f22d55c02b7a5ea4047731d5c7083f9e97615dc
AlexKutsyk <AKutsyk@yandex.ru>
2024-06-16 01:12:06 +0300

    Fix bugs

--- app/src/main/java/ru/practicum/android/diploma/favorites/domain/impl/FavoriteInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/impl/FavoriteInteractorImpl.kt
@@ -1,9 +1,3 @@
-package ru.practicum.android.diploma.favorites.domain.impl
-
-import ru.practicum.android.diploma.favorites.domain.api.FavoriteRepository
-import ru.practicum.android.diploma.favorites.domain.api.FavoriteInteractor
-import ru.practicum.android.diploma.vacancy.domain.models.VacancyDetails
-import kotlinx.coroutines.flow.Flow
 
 class FavoriteInteractorImpl(private val favoriteRepository: FavoriteRepository) : FavoriteInteractor {
     override suspend fun insertFavoriteVacancy(vacancy: VacancyDetails) {
