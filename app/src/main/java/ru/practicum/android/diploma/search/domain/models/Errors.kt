276433d038e69f62d61b5bc94a465d95c9ae197c
AlexKutsyk <AKutsyk@yandex.ru>
2024-06-17 17:56:54 +0300

    Fix bugs

--- app/src/main/java/ru/practicum/android/diploma/search/domain/models/Errors.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/models/Errors.kt
@@ -3,6 +3,6 @@ package ru.practicum.android.diploma.search.domain.models
 sealed class Errors {
     data object ConnectionError : Errors()
     data object IncorrectRequest : Errors()
-    data object ServerError: Errors()
+    data object ServerError : Errors()
     data object Error404 : Errors()
 }
