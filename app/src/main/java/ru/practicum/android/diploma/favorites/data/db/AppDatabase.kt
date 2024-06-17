cb6045c68b68c36044630774e93e314eb92f7cfb
AlexKutsyk <AKutsyk@yandex.ru>
2024-06-17 18:06:13 +0300

    Change version=2 DB

--- app/src/main/java/ru/practicum/android/diploma/favorites/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import ru.practicum.android.diploma.favorites.data.db.dao.VacancyDao
 import ru.practicum.android.diploma.favorites.data.db.entity.VacancyEntity
 
-@Database(version = 1, entities = [VacancyEntity::class])
+@Database(version = 2, entities = [VacancyEntity::class])
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun vacancyDao(): VacancyDao
